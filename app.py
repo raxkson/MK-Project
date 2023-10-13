35f5707cb599665d41623e5f8e595cf1c25dff63
raxkson <raxkson@gmail.com>
2023-10-13 15:42:14 +0900

    Fix sample app.py use DIL

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from MK.common import set_data
-from MK.common import set_rule
-def main():
-    set_data
+import pandas
+# from DIL import Suppression
+# from DIL import
 
+import DIL
+
+
+def main():
+    datas = pandas.read_csv('../data/test_100.csv')
+    # Suppression 삭제
+    # DIL.suppression.Suppression(datas).general(["이름"])
+    # Suppression 부분 삭제
+    # DIL.suppression.Suppression(datas).local("이름",[1,2])
+    # Suppression 마스킹 ex) 임*은
+    # DIL.suppression.Suppression(datas).masking("이름", [1,2])
+    print(datas.head())
 
 
 if __name__ == "__main__":
